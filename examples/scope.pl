f4033b1f9d0a9c737e130805f064785da2717381
Sjors Gielen <sjors@sjorsgielen.nl>
2015-02-26 21:20:30 +0100

    In scope.pl, report via stdout what is the expected result.

--- examples/scope.pl
+++ examples/scope.pl
@@ -17,22 +17,21 @@ $dazeus->setProperty("examples.scope.foo", "baz", "oftc");
 print "Set network scope for 'oftc' to baz\n";
 
 my $value = $dazeus->getProperty("examples.scope.foo", "q", "#moo");
-print "Value for network scope 'q', channel #moo: $value\n"; # should be bar
+print "Value for network scope 'q', channel #moo (should be bar): $value\n";
 $value = $dazeus->getProperty("examples.scope.foo", "oftc", "#moo");
-print "Value for network scope 'oftc', channel #moo: $value\n"; # should be baz
+print "Value for network scope 'oftc', channel #moo (should be baz): $value\n";
 
 $dazeus->unsetProperty("examples.scope.foo", "oftc");
 print "Unset network scope for 'oftc'\n";
 $value = $dazeus->getProperty("examples.scope.foo", "oftc", "#moo");
-print "Value for network scope 'oftc', channel #moo: $value\n"; # should be bar
+print "Value for network scope 'oftc', channel #moo (should be bar): $value\n";
 $dazeus->unsetProperty("examples.scope.foo");
 print "Unset global scope\n";
 
 $dazeus->setProperty("examples.scope.foo", "quux", "oftc");
 print "Set network scope 'oftc' to quux\n";
 $value = $dazeus->getProperty("examples.scope.foo", "q");
-print "Network value for network 'q': " . (defined $value ? $value : "unset") . "\n";
-# should be unset
+print "Network value for network 'q' (should be unset): " . (defined $value ? $value : "unset") . "\n";
 
 # Clean up
 $dazeus->unsetProperty("examples.scope.foo", "oftc");
